09e075bba14bb9d4f647d8a0ca16caa2519dd30a
Ron <ronmwangi00@gmail.com>
2023-06-24 10:12:14 +0300

    Completed  Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener('DOMContentLoaded', () => {
+  let form = document.querySelector('form')
+  let todoText = document.querySelector('#todo-text')
+  let todosList = document.querySelector('#todos-list')
+  let List = document.querySelector('#list')
+
+
+ 
+
+  form.addEventListener('submit', (e) => {
+      e.preventDefault()
+      let NewToDo = todoText.value;
+      let listItem = document.createElement('li');
+      listItem.textContent = NewToDo;
+      List.appendChild(listItem);
+      e.target.reset()
+  })
+})
